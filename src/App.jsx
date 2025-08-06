3cb22c31a3ef297ad93623611fe9c97a8906aa4f
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-08-06 11:18:43 +0430

    Initial Updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,55 @@
 import React, { useState } from "react";
-import Header from "./components/Header";
 import Shop from "./components/Shop";
 import { DUMMY_PRODUCTS } from "./dummy-products";
-import { CartContext } from './store/shopping-cart-context';
-import Product from "./components/Product";
-import CartContextProvider from "./store/shopping-cart-context";
+import Header from './components/Header';
   
 
 function App() {
-    return (
-        <CartContextProvider>
-            <Header />
+    const [shoppingCart, setShoppingCart] = useState({
+        items: [],
+    });
+    console.log(shoppingCart)
 
-            <Shop>
-                {DUMMY_PRODUCTS.map((product) => (
-                    <li key={product.id}>
-                        <Product 
-                            {...product}
-                        />
-                    </li>   
-                ))}
-            </Shop>
-        </CartContextProvider>
+    function handleAddItemToCart(id) {
+        setShoppingCart((prevShoppingCart) => {
+            const updatedItems = [...prevShoppingCart.items];
+
+            const existingCartItemIndex = updatedItems.findIndex(
+                (cartItem) => cartItem.id === id
+            );
+            
+            const existingCartItem = updatedItems[existingCartItemIndex];
+
+            if (existingCartItem) {
+                const updatedItem = {
+                    ...existingCartItem,
+                    quantity: existingCartItem.quantity + 1,
+                };
+                updatedItems[existingCartItemIndex] = updatedItem;
+            } else {
+                const product = DUMMY_PRODUCTS.find((product) => product.id === id);
+                updatedItems.push({
+                    id: id,
+                    name: product.title,
+                    price: product.price,
+                    quantity: 1,
+                });
+            };
+
+            return {
+                items: updatedItems,
+            };
+        });
+    };
+
+
+    return (
+        <>
+            <Header 
+                cart={shoppingCart}
+            />
+            <Shop onAddItemToCart={handleAddItemToCart} />
+        </>
     );
 };
 
