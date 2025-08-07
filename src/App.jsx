6cdd2c6f17ce2b06b212a31d2d733326fad54238
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-08-07 11:22:34 +0430

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,52 +5,76 @@ import Header from './components/Header';
   
 
 function App() {
-    const [shoppingCart, setShoppingCart] = useState({
-        items: [],
-    });
-    console.log(shoppingCart)
+  const [shoppingCart, setShoppingCart] = useState({
+    items: [],
+  });
 
-    function handleAddItemToCart(id) {
-        setShoppingCart((prevShoppingCart) => {
-            const updatedItems = [...prevShoppingCart.items];
+  function handleAddItemToCart(id) {
+    setShoppingCart((prevShoppingCart) => {
+      const updatedItems = [...prevShoppingCart.items];
+    //   Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum quia sit atque fugiat tempore perspiciatis quisquam saepe vel non iure odit, amet magni repellat dolores officia neque sequi temporibus enim.
 
-            const existingCartItemIndex = updatedItems.findIndex(
-                (cartItem) => cartItem.id === id
-            );
-            
-            const existingCartItem = updatedItems[existingCartItemIndex];
+      const existingCartItemIndex = updatedItems.findIndex(
+        (cartItem) => cartItem.id === id
+      );
+      const existingCartItem = updatedItems[existingCartItemIndex];
 
-            if (existingCartItem) {
-                const updatedItem = {
-                    ...existingCartItem,
-                    quantity: existingCartItem.quantity + 1,
-                };
-                updatedItems[existingCartItemIndex] = updatedItem;
-            } else {
-                const product = DUMMY_PRODUCTS.find((product) => product.id === id);
-                updatedItems.push({
-                    id: id,
-                    name: product.title,
-                    price: product.price,
-                    quantity: 1,
-                });
-            };
-
-            return {
-                items: updatedItems,
-            };
+      if (existingCartItem) {
+        const updatedItem = {
+          ...existingCartItem,
+          quantity: existingCartItem.quantity + 1,
+        };
+        updatedItems[existingCartItemIndex] = updatedItem;
+      } else {
+        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
+        updatedItems.push({
+          id: id,
+          name: product.title,
+          price: product.price,
+          quantity: 1,
         });
-    };
+      }
 
+      return {
+        items: updatedItems,
+      };
+    });
+  }
 
-    return (
-        <>
-            <Header 
-                cart={shoppingCart}
-            />
-            <Shop onAddItemToCart={handleAddItemToCart} />
-        </>
-    );
-};
+  function handleUpdateCartItemQuantity(productId, amount) {
+    setShoppingCart((prevShoppingCart) => {
+      const updatedItems = [...prevShoppingCart.items];
+      const updatedItemIndex = updatedItems.findIndex(
+        (item) => item.id === productId
+      );
+
+      const updatedItem = {
+        ...updatedItems[updatedItemIndex],
+      };
+
+      updatedItem.quantity += amount;
+
+      if (updatedItem.quantity <= 0) {
+        updatedItems.splice(updatedItemIndex, 1);
+      } else {
+        updatedItems[updatedItemIndex] = updatedItem;
+      }
+
+      return {
+        items: updatedItems,
+      };
+    });
+  }
+
+  return (
+    <>
+      <Header
+        cart={shoppingCart}
+        onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
+      />
+      <Shop onAddItemToCart={handleAddItemToCart} />
+    </>
+  );
+}
 
 export default App;
