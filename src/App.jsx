3baf8fad92fad03ef3fc72548371c770cf481f8e
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-08-24 14:51:49 +0430

    First Initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
 import React, { useState } from "react";
-import Shop from "./components/Shop";
-import { DUMMY_PRODUCTS } from "./dummy-products";
-import Header from './components/Header';
-import Product from "./components/Product";
-import CartContextProvider from "./store/shopping-cart-context";
   
 
 function App() {
   return (
-    <CartContextProvider>
-      <Header />
-      <Shop>
-        {DUMMY_PRODUCTS.map((product) => (
-          <li key={product.id}>
-            <Product {...product} />
-          </li> 
-        ))}
-      </Shop>
-    </CartContextProvider>
+    <>
+    </>
   );
 };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,46 +1,7 @@
-import { useContext, useRef } from 'react';
-import logo from '../assets/logo.png';
-import CartModal from './CartModal';
-import { CartContext } from '../store/shopping-cart-context';
 
-
-export default function Header({ onUpdateCartItemQuantity }) {
-  const modal = useRef();
-  const { items } = useContext(CartContext);
-
-  const cartQuantity = items.length;
-
-  function handleOpenCartClick() {
-    modal.current.open();
-  };
-
-  let modalActions = <button>Close</button>;
-
-  if (cartQuantity > 0) {
-    modalActions = (
-      <>
-        <button>Close</button>
-        <button>Checkout</button>
-      </>
-    );
-  };
-
+export default function Header() {
   return (
-    <>
-      <CartModal
-        ref={modal}
-        title="Your Cart"
-        actions={modalActions}
-      />
-      <header id="main-header">
-        <div id="main-title">
-          <img src="logo.png" alt="Elegant model" />
-          <h1>Elegant Context</h1>
-        </div>
-        <p>
-          <button onClick={handleOpenCartClick}>Cart ({cartQuantity})</button>
-        </p>
-      </header>
+      <>
     </>
   );
 }
