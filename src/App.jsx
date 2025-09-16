121fe696de002069beb0e5272b7c458ba96324a6
Shakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-09-16 22:23:17 +0430

    Function Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,47 @@
 import React, { useState } from 'react';
-import Header from './components/Header';
 import Shop from './components/Shop';
-import { DUMMY_PRODUCTS } from './dummy-products';
-import Product from './components/Product';
-import CartContextProvider from './store/shopping-cart-context';
+import DUMMY_PRODUCTS from './dummy-products';
 
 
 function App() {
+    const [shoppingCart, setShoppingCart] = useState({
+        items: [],
+    });
+
+    function handleAddItemToCart(id) {
+        setShoppingCart((prevShoppingCart) => {
+            const updatedItems = [...prevShoppingCart.items];
+
+            const existingCartItemIndex = updatedItems.findIndex(
+                (cartItem) => cartItem.id === id
+            );
+            const existingCartItem = updatedItems[existingCartItemIndex];
+
+            if (existingCartItem) {
+                const updatedItem = {
+                    ...existingCartItem,
+                    quantity: existingCartItem.quantity + 1,
+                };
+                updatedItems[existingCartItemIndex] = updatedItem;
+            } else {
+                const product = DUMMY_PRODUCTS.find((product) => product.id === id);
+                updatedItems.push({
+                    id: id,
+                    name: product.title,
+                    price: product.price,
+                    quantity: 1,
+                });
+            };
+
+            return {
+                items: updatedItems,
+            };
+        });
+    };
+
     return (
         <>
+            <Shop onAddItemToCart={handleAddItemToCart} />
         </>
     );
 };
