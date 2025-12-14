c3f7c0c62370c37e541bebd52e8d22f87f05806d
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-12-14 13:55:25 +0430

    first initial

--- src/Dependencies.jsx
+++ src/Dependencies.jsx
@@ -1,55 +1,30 @@
-import React, { useEffect, useState } from "react";
+import React, { useRef, useState } from "react";
 
 
-function Dependencies() {
-    const [count, setCount] = useState(1);
-    const [show, setShow] = useState(false);
-    const [countries, setCountries] = useState(["India", "USA", "Pakistan"]);
-    const [user, setUser] = useState({ id: 1, name: "Harry", age: 15 }); 
+function Basic() {
+    const [name, setName] = useState("");
 
-    const increase = () => {
-        setCount(count + 1);
+    const inputChange = (e) => {
+        setName(e.target.value);
     };
-
-    const showContent = () => {
-        setShow(true);
-    };
-
-    const addCountry = () => {
-        // countries.push("Germany");
-        // setCountries(countries);
-        setCountries((prev) => [...prev, "Germany"]);
-    };
-
-    const changeUser = () => {
-        user.age = 17;
-        setUser(user);
-    };
-
-    useEffect(() => {
-        console.log("UserEffect is triggered");
-    }, [count, show, countries, user]);
 
     return (
-        <>
+        <div>
             <div>
-                <button onClick={increase}>Increase</button>
-                <button onClick={showContent}>Show</button>
-                <button onClick={addCountry}>Add Country</button>
-                <button onClick={changeUser}>Change User</button>
+                <label>Username</label>
+                <input type="text" onChange={inputChange} />
             </div>
 
-            <div>The count is {count}</div>
-
-            {show ? <div>This content is shown</div> : null}
+            <div>
+                <label>Password</label>
+                <input type="password" />
+            </div>
 
             <div>
-                {countries.map((country, index) => (
-                    <div key={index}>{country}</div>
-                ))}
+                <button>Login</button>
             </div>
-        </>
+        </div>
     );
 };
 
-export default Dependencies;
+export default Basic;
