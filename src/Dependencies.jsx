eabde9f4a2823c060e1d9c5185f0cc8482cd9055
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-12-14 09:34:33 +0430

    Initial Commit

--- src/Dependencies.jsx
+++ src/Dependencies.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 function Dependencies() {
+    const [count, setCount] = useState(1);
+    const [show, setShow] = useState(false);
+    const [countries, setCountries] = useState(["India", "USA", "Pakistan"]);
+    const [user, setUser] = 
+
+    const increase = () => {
+        setCount(count + 1);
+    };
+
+    const showContent = () => {
+        setShow(true);
+    };
+
+    const addCountry = () => {
+        countries.push("Germany");
+        setCountries(countries);
+    };
+
+    const changeUser = () => {
+        UserActivation.
+    }
+
     return (
         <div>
             Dependencies
