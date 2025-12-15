3a89d519fa4be421d1f023518127ac9992fb1ed6
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-12-15 07:52:07 +0430

    Initial Commit

--- src/Dependencies.jsx
+++ src/Dependencies.jsx
@@ -2,27 +2,38 @@ import React, { useRef, useState } from "react";
 
 
 function Basic() {
-    const [name, setName] = useState("");
+    // const [name, setName] = useState("");
 
-    const inputChange = (e) => {
-        setName(e.target.value);
+    const unameRef = useRef("");
+    const pwordRef = useRef("");
+
+    // const inputChange = (e) => {
+    //     setName(e.target.value);
+    // };
+
+    const login = () => {
+        console.log(unameRef.current.value);
+        console.log(pwordRef.current.value);
     };
 
     return (
         <div>
             <div>
                 <label>Username</label>
-                <input type="text" onChange={inputChange} />
+                {/* <input type="text" onChange={inputChange} /> */}
+                <input type="text" ref={unameRef} />
             </div>
 
             <div>
                 <label>Password</label>
-                <input type="password" />
+                <input type="password" ref={pwordRef} />
             </div>
 
             <div>
-                <button>Login</button>
+                <button onClick={login}>Login</button>
             </div>
+
+            {/* <div>{name}</div> */}
         </div>
     );
 };
