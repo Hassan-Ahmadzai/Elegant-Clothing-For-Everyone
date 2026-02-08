851bdc8dad731e08a8691f8d0c02d8125be45cb5
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2026-02-08 07:57:07 +0430

    Function Updated

--- src/App.jsx
+++ src/App.jsx
@@ -10,36 +10,7 @@ function App() {
         items: [],
     });
 
-    function handleAddItemToCart(id) {
-        setShoppingCart((prevShoppingCart) => {
-            const updatedItems = [...prevShoppingCart.items];
-
-            const existingCartItemIndex = updatedItems.findIndex(
-                (cartItem) => cartItem.id === id
-            );
-            const existingCartItem = updatedItems[existingCartItemIndex];
-
-            if (existingCartItem) {
-                const updatedItem = {
-                    ...existingCartItem,
-                    quantity: existingCartItem.quantity + 1,
-                };
-                updatedItems[existingCartItemIndex] = updatedItem;
-            } else {
-                const product = DUMMY_PRODUCTS.find((product) => product.id === id);
-                    updatedItems.push({
-                    id: id,
-                    name: product.title,
-                    price: product.price,
-                    quantity: 1,
-                });
-            };
-
-            return {
-                items: updatedItems,
-            };
-        });
-    };
+    
 
     function handleUpdateCartItemQuantity(productId, amount) {
         setShoppingCart((prevShoppingCart) => {
