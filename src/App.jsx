43a38e507b85a2655d59a63f8312e6b189791044
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2026-02-19 10:00:22 +0430

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -53,19 +53,19 @@ function App() {
                             image={CORE_CONCEPTS[0].image}
                         />
                         <CoreConcept 
-                            title={CORE_CONCEPTS[0].title} 
-                            description={CORE_CONCEPTS[0].description} 
-                            image={CORE_CONCEPTS[0].image}
+                            title={CORE_CONCEPTS[1].title} 
+                            description={CORE_CONCEPTS[1].description} 
+                            image={CORE_CONCEPTS[1].image}
                         />
                         <CoreConcept 
-                            title={CORE_CONCEPTS[0].title} 
-                            description={CORE_CONCEPTS[0].description} 
-                            image={CORE_CONCEPTS[0].image}
+                            title={CORE_CONCEPTS[2].title} 
+                            description={CORE_CONCEPTS[2].description} 
+                            image={CORE_CONCEPTS[2].image}
                         />
                         <CoreConcept 
-                            title={CORE_CONCEPTS[0].title} 
-                            description={CORE_CONCEPTS[0].description} 
-                            image={CORE_CONCEPTS[0].image}
+                            title={CORE_CONCEPTS[3].title} 
+                            description={CORE_CONCEPTS[3].description} 
+                            image={CORE_CONCEPTS[3].image}
                         />
                     </ul>
                 </section>
