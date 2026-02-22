281f0ecde1a0fe3bcfe1de134b78e1f304e541be
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2026-02-22 11:48:44 +0430

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,30 @@ function App() {
     return (
         <div>
             <Header />
-            
+            <main>
+                <section id="core-concepts">
+                    <h2>Time to get started!</h2>
+
+                    <ul>
+                        <CoreConcept {...CORE_CONCEPTS[0]} />
+                        <CoreConcept {...CORE_CONCEPTS[1]} />
+                        <CoreConcept {...CORE_CONCEPTS[2]} />
+                        <CoreConcept {...CORE_CONCEPTS[3]} />
+                    </ul>       
+                </section>
+
+                <section id="examples">
+                    <h2>Examples</h2>
+                    <menu>
+                        <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+                        <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+                        <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+                        <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+                    </menu>
+
+                    Dynamic Content
+                </section>
+            </main>
         </div>
     );
 };
