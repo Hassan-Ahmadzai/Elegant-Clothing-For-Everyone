849c15f37c300a01c997a2090ae2e44e262e889f
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2026-03-03 09:05:50 +0430

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     const [activePlayer, setActivePlayer] = useState('X');
 
     function handleSelectSquare() {
-        setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');
+        setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X');  
     };
 
     return (
