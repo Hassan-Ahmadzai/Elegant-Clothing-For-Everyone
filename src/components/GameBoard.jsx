871dcdf43a89e2347fd534e83b7f5ad8d10219de
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2026-03-03 08:57:01 +0430

    Initial Commit

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,46 @@
+
+import { useState } from "react";
+
+const initialGameBoard = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+];
+
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {   
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+    console.log("gameBoard: ", gameBoard);
+    
+    function handleSelectSquare(rowIndex, colIndex) {
+        // console.log("rowIndex: ", rowIndex, " colIndex: ", colIndex);
+        setGameBoard((prevGameBoard) => {
+            const updatedBoard = [...prevGameBoard.map((innerArray) => [...innerArray])];
+            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+            return updatedBoard;
+        });
+
+        onSelectSquare();
+    };
+
+    return (
+        <ol id="game-board">
+            {gameBoard.map((row, rowIndex) => (
+                    // console.log("row: ", row, " rowIndex: ", rowIndex)
+                <li key={rowIndex}>
+                    <ol>
+                        {row.map((playerSymbol, colIndex) => (
+                            // console.log("playerSymbol: ", playerSymbol, " colIndex: ", colIndex),
+                            <li key={colIndex}>
+                                <button 
+                                    onClick={() => handleSelectSquare(rowIndex, colIndex)}
+                                >
+                                    {playerSymbol}
+                                </button>
+                            </li>
+                        ))}
+                    </ol>
+                </li>
+            ))}
+        </ol>
+    );
+};
